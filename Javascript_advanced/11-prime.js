46b45b5ed9135b6d8e01365274d4d0deb2f6d91a
Fpierr <fanuelp9@gmail.com>
2025-01-26 19:53:55 +0100

    use setTimeout

---
+++ Javascript_advanced/11-prime.js
@@ -0,0 +1,28 @@
+function countPrimeNumbers() {
+    let countPrime = 0
+    for(let number = 2; number <= 100; number++){
+        let isPrime = true
+        for(let i = 2; i <= Math.sqrt(number); i++) {
+            if(number % i === 0) {
+                isPrime = false
+                break
+            }
+        }
+        if (isPrime) {
+            countPrime++
+        }
+    }
+    return countPrime
+}
+
+const iteration = 100
+
+setTimeout(() => {
+    const start = performance.now()
+    for(let i = 0; i < iteration; i++) {
+        countPrimeNumbers()
+    }
+    const end = performance.now()
+    console.log(`Execution time of calculating prime numbers 100 times was ${end - start} milliseconds.`)
+},0)
+
